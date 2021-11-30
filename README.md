d6402c5a45465a0bfa3c1c6faee468e71bc7b72d
Valkryst <valkryst@valkryst.com>
2021-11-30 14:31:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+## Installation
+
+To download this script and its dependencies, copy and paste the following commands into your bash terminal.
+
+```bash
+# Install Dependencies
+sudo apt update
+sudo apt install ffmpeg wget
+
+
+
+# Download Script
+sudo wget https://github.com/Valkryst/Automated_Video_Conversion/blob/main/convert.sh
+
+
+
+# Modify the script's permissions to allow it to be run.
+sudo chmod +x convert.sh
+```
+
+With the script downloaded, you must then perform these steps:
+
+* Move the script into its own folder.
+  
+  * e.g. `/home/conversion/video`
+
+* Edit the script and make the following changes:
+  
+  * Change `/home/conversion/video` in the command  `cd /home/conversion/video` to point to the folder in which you placed the script.
+  
+  * Change `ubuntu` in the command `chown -R ubuntu "$folder_path"` to your username.
+  
+  * Change `ubuntu` in the command `chgrp -R ubuntu "$folder_path"` to your username.
+
+* Run the script to generate all necessary subfolders.
+
+* Add a [cron]([cron - Wikipedia](https://en.wikipedia.org/wiki/Cron)) job to run the script at a set interval.
+  
+  * e.g. `0 * * * * /bin/bash /home/conversion/video/convert.sh > /home/conversion/video/convert.log`
+
+## Usage
+
+Place files and folders in the `todo` folder and they will be converted by the cron job. You'll know a file has been completed, if the original file has been moved to `completed/original_files`.
+
+After verifying that the converted file is as you expect, you can delete the original file from `completed/original_files`.
+
+## Important Notes
+
+* Any folders containing the word `sample` are deleted, along with all files within those folders.
+
+* Any files ending in `.nfo` or `.txt` are deleted.
---
+++ convert.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+cd /home/conversion/video
+
+
+
+# Prevent multiple instances of the script from being run.
+lockfile="lockfile.tmp"
+if [ -f "$lockfile" ]; then
+        exit
+fi
+touch "$lockfile"
+
+
+
+# Ensure Required Folders Exist
+folder_todo="$(pwd)/todo"
+folder_complete="$(pwd)/complete"
+folder_original_files="$(pwd)/complete/original_files"
+
+folders=( "$folder_todo" "$folder_complete" "$folder_original_files" )
+for folder_path in "${folders[@]}"
+do
+        if [[ ! -d "$folder_path" ]]; then
+                mkdir "$folder_path"
+                echo "Created $folder_path"
+        fi
+done
+
+
+
+# Delete Unnecessary Files & Folders
+unnecessary_folder_patterns=( ".*[Ss][Aa][Mm][Pp][Ll][Ee].*" )
+for pattern in "${unnecessary_folder_patterns[@]}"
+do
+        find "$folder_todo" -type d -regex "$pattern" | while read folder_path; do
+                rm --recursive "$folder_path"
+                echo "Deleted $folder_path"
+        done
+done
+
+unnecessary_file_patterns=( ".*\.nfo" ".*\.txt" )
+for pattern in "${unnecessary_file_patterns[@]}"
+do
+        find "$folder_todo" -type f -regex "$pattern" | while read file_path; do
+                rm "$file_path"
+                echo "Deleted $file_path"
+        done
+done
+
+
+
+# Convert files.
+extensions=( "avi" "divx" "flx" "m2ts" "m4v" "mkv" "mov" "mp4" "mpg" "ts" "webm" "wmv" )
+
+for extension in "${extensions[@]}"
+do
+        find "$folder_todo" -regex ".*$extension" | while read file_path; do
+                file_name="$(basename "$file_path")"
+                output_folder="$folder_complete/$(basename $(dirname $file_path))"
+
+                if [[ ! -d "$output_folder" ]]; then # Ensure Output Folder Exists
+                        mkdir "$output_folder"
+                        echo "Created $output_folder"
+                fi
+
+                echo "Starting Conversion of $file_path"
+                ffmpeg -i "$file_path" \
+                        -codec:v libx265 \
+                        -x265-params crf=10 \
+                        -x265-params psy-rd=1 \
+                        -c:a copy \
+                        -c:s copy \
+                        -map 0 \
+                        -preset medium \
+                        -threads 4 \
+                        "$output_folder/$file_name.mkv" -y
+                echo "Completed Conversion of $file_path"
+
+                mv "$(dirname $file_path)" "$folder_original_files"
+                echo "Moved \"$(dirname $file_path)\" to \"$folder_original_files\""
+        done
+done
+
+
+
+# Ensure the primary user is owner of all files/folders.
+for folder_path in "${folders[@]}"
+do
+        chown -R ubuntu "$folder_path"
+        chgrp -R ubuntu "$folder_path"
+done
+
+
+
+rm $lockfile
